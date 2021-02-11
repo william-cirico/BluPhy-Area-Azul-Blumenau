ee6b3681c1296ae5182abf3f609b7badd566c4e9
WILLIAM CIRICO <william.cirico@alunos.sc.senac.br>
2021-02-11 14:43:12 -0300

    update

--- Software/front-end/AreaAzulFuncionarios/src/screens/Teste.js
+++ Software/front-end/AreaAzulFuncionarios/src/screens/Teste.js
@@ -1,20 +1,98 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
-import { StyleSheet, Text, View  } from 'react-native';
+import { StyleSheet, Text, View, Dimensions  } from 'react-native';
 
-//import Car from 'react-native-vector-icons/FontAwesome';
-//import Motorcyle from 'react-native-vector-icons/MaterialIcons';
+import Car from 'react-native-vector-icons/FontAwesome';
+import Motorcyle from 'react-native-vector-icons/MaterialIcons';
 
 import commonStyles from '../theme/commonStyles';
 
 
-export default props => {
+export default ({ navigation }) => {    
+    const reducer = (prevState, action) => {        
+        switch(action.type) {
+            case 'CAR':
+                return {
+                    ...prevState,
+                    isCarChecked: true,
+                    isMotorcycleChecked: false,
+                }
+            case 'MOTORCYCLE':
+                return {
+                    ...prevState,
+                    isCarChecked: false,
+                    isMotorcycleChecked: true,
+                    
+                }
+            case 'LICENSE_PLATE':                            
+                if (licensePlateRegex.test(action.licensePlate)) {                    
+                    return {
+                        ...prevState,
+                        islicensePlateValid: true,
+                        licensePlate: action.licensePlate,                        
+                    }
+                } else {
+                    return {
+                        ...prevState,
+                        islicensePlateValid: false,
+                        licensePlate: action.licensePlate,
+                    }
+                }
+            case 'VEHICLE_MODEL':
+                if (action.vehicleModel.trim().length >= 6) {
+                    return {
+                        ...prevState,
+                        isVehicleModelValid: true,                    
+                        vehicleModel: action.vehicleModel,
+                    }
+                } else {
+                    return {
+                        ...prevState,
+                        isVehicleModelValid: false,                    
+                        vehicleModel: action.vehicleModel,
+                    }
+                }
+        }
+    };
+
+    initialState = {
+        isCarChecked: true,
+        isMotorcycleChecked: false,
+        licensePlate: 'BRA2E19',
+        islicensePlateValid: false,
+        vehicleModel: '',
+        isVehicleModelValid: false,
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const validations = [
+        state.islicensePlateValid, 
+        state.isVehicleModelValid,
+    ]
+
+    const validForm = validations.reduce((acc, cv) => acc && cv);
+
+    const addVehicle = () => {
+        //TODO: Implementar
+        navigation.navigate('MainScreen')
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.containerInformation}>
                 <View style={styles.containerVehicle}>
                     <View style={styles.boxVehicle}>
-                        
+                        <Car
+                            name='car'
+                            size={50}
+                            color='white'
+                        />
+                        <Motorcyle
+                            name='motorcycle'
+                            size={50}
+                            color='white'
+                        />
                     </View>
                 </View>
             </View>
@@ -54,13 +132,17 @@ const styles = StyleSheet.create({
         width: 220,
         borderWidth: 2,
         borderColor: '#1253aa',
-        marginVertical: 20,
-        padding: 20,           
-        borderRadius: 10,
+        marginVertical: 20,  
+        borderRadius: 25,
     },
     boxVehicle: {
         backgroundColor: '#1253aa',
-        borderWidth: 2,
+        width: 115,
+        height: 108,
+        borderRadius: 20,
+        borderColor: '#1253aa',
+        justifyContent: 'center',
+        paddingLeft: 25,
     },
     bodyContainer: {
         flex: 3, 
